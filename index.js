fb8bdcd65f1bf9debd68d0263df5276d8e3afb27
Ramssesk <ramsses_vm@hotmail.com>
2021-12-02 18:49:38 -0600

    first comimmit

---
+++ index.js
@@ -0,0 +1,32 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const cors = require("cors");
+
+const port = process.envPORT || 4000;
+app.use(cors({
+    origin: "*",
+    methods: "GET, POST",
+}));
+
+app.get("/", (req, res) => {
+    res.send('server is running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(port, () => console.log('listen: ', port))
