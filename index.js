ce0842bc1d250d26770211c011391f9ed9cc8657
Ramssesk <ramsses_vm@hotmail.com>
2021-12-02 19:32:35 -0600

    commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const app = require("express")();
+const express = require("express")
+const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 const cors = require("cors");
 
 const port = process.envPORT || 4000;
-app.use(cors());
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+app.use(cors({
+    origin: '*', 
+    methods: ['GET','POST']
+}));
+app.use(express.json({extended: true}));
 
 app.get("/", (req, res) => {
     res.send('server is running');
@@ -14,15 +24,15 @@ io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
 
 	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
+		socket.broadcast.emit("callended")
 	});
 
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("calluser", { signal: signalData, from, name });
 	});
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
+	socket.on("answercall", (data) => {
+		io.to(data.to).emit("callaccepted", data.signal)
 	});
 });
 
